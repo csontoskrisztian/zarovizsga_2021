df8f735be6d90660ed0fc0ecac4b68f69d5f81e5
Csontos Krisztián <csontos.krisztian11@gmail.com>
2021-02-11 18:15:36 +0100

    8x8 tábla legenrálása

--- my.js
+++ my.js
@@ -3,13 +3,20 @@ $(() => {
     $table.width(400);
     $table.height(400);
     let tableC = $table[0].getContext('2d');
+    tableC.fillStyle = " #b3b3b3";
+    tableC.fillRect(0, 0, $table.width(), $table.height())
 
     let img = new Image;
-    img.src = "./img/Apple.png";
-
+    let img_width = 50;
+    let img_height = img_width / 2;
+    
     img.onload = function() {
-        // Lehetséges egy képet több részre vágni, tehát a dolgokat egy nagy képen tárolni, betölteni, majd megjelenítésnél felvágni és külön-külön használni
-        tableC.drawImage(img, 90, 130, 50, 60, 10, 10, 50, 60);
+        for (let x = 0; x < 8; x++) {
+            for (let y = 0; y < 8; y++) {
+                tableC.drawImage(img, 0 + (img_width * x), 0 + (img_height * y), img_width, img_height);
+            }
+        }
     };
-
+    
+    img.src = "./img/Apple.png";
 })
