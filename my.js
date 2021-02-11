5bf8c59a2dd6a78478e30bdf092ea044a7882f6b
Csontos Krisztián <csontos.krisztian11@gmail.com>
2021-02-11 17:57:15 +0100

    Felvágás teszt

--- my.js
+++ my.js
@@ -1,13 +1,15 @@
 $(() => {
     let $table = $("#Table");
-    $table.width(300);
+    $table.width(400);
+    $table.height(400);
     let tableC = $table[0].getContext('2d');
 
     let img = new Image;
     img.src = "./img/Apple.png";
 
     img.onload = function() {
-        tableC.drawImage(img, 10, 10, 100, 100);
+        // Lehetséges egy képet több részre vágni, tehát a dolgokat egy nagy képen tárolni, betölteni, majd megjelenítésnél felvágni és külön-külön használni
+        tableC.drawImage(img, 90, 130, 50, 60, 10, 10, 50, 60);
     };
 
 })
